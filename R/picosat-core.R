7e2eaea17f60b357d439c6bf9895d210a1387347
mike <mikefc@coolbutuseless.com>
2025-04-05 18:12:22 +1000

    include option to 'keep_dummy' when solving satire problems

--- R/picosat-core.R
+++ R/picosat-core.R
@@ -31,6 +31,8 @@ pico_solve_literals <- function(literals, max_solutions = 1) {
 #' @inheritParams pico_solve_literals
 #' @param sat SAT problem definition as created by \code{satire::sat_new()} or
 #'        \code{satire::read_dimacs()}
+#' @param keep_dummy should variables starting with the prefix 'dummy' be 
+#'        dropped from the solution?  Default: FALSE
 #' @return Return NULL is problem is unsatisfiable.  Otherwise return a data.frame
 #'         of solutions where each column represents a named variable in the 
 #'         problem and each row is a solution.
@@ -42,7 +44,7 @@ pico_solve_literals <- function(literals, max_solutions = 1) {
 #' satire::sat_solve_dpll(sat, max_solutions = 10)
 #' @export
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-pico_solve_satire <- function(sat, max_solutions = 1) {
+pico_solve_satire <- function(sat, max_solutions = 1, keep_dummy = FALSE) {
   
   stopifnot(inherits(sat, "sat_prob"))
   non_dummy_idxs <- which(!grepl("^dummy", sat$names)) - 1L
@@ -52,7 +54,7 @@ pico_solve_satire <- function(sat, max_solutions = 1) {
   
   # Map to named logicals
   res <- lapply(res, function(soln) {
-    satire::sat_literals_to_lgl(sat, soln)
+    satire::sat_literals_to_lgl(sat, soln, keep_dummy = keep_dummy)
   })
   
   # Convert to data.frrame
